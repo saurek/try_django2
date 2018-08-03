c3e3f1dbd8105c042013bd8b991175885be9c0db
saurek <s.reklaitis@gmail.com>
2018-08-03 22:19:19 +0300

    1 - Added Product app

---
+++ src/products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+    summary = models.TextField(default='111')
