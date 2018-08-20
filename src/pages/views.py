90bf8c4e4468f6be936bd6f892a8140aad5a7a4b
saurek <s.reklaitis@gmail.com>
2018-08-20 16:49:28 +0300

    5 - Starting to work on Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,16 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hail Satan</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
 
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
 
 
 def social_view(*args, **kwargs):
